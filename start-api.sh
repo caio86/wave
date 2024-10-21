be58a3843a10477c8de6d842eaa94f049099a5a4
caio86 <caioluiz86@gmail.com>
2024-10-21 20:11:49 -0300

    refactor: fix styling

--- start-api.sh
+++ start-api.sh
@@ -2,30 +2,30 @@
 
 # ./start-api.sh
 
-dpkg --list | grep "venv" > /dev/null 
+# dpkg --list | grep "venv" > /dev/null
 
-if [ $? -ne 0 ]; then
-   echo -e "❌  package python3-venv is not installed!\n"
-   exit 1
+if [ -d venv ]; then
+
+        echo -e "🐍  Activating Python virtual environment... "
+        source venv/bin/activate
+        pip3 freeze | grep -E "flask-restx" >/dev/null
+        if [ ! $? -ne 0 ]; then
+                echo "📦  Installing API dependencies... "
+                pip3 install flask flask-restx python-dotenv requests >/dev/null
+        fi
+else
+        echo "🕒 Craeting Python virtual environment... "
+        python3 -m venv venv
+        echo -e "🐍  Activating Python virtual environment... "
+        source venv/bin/activate
+
+        echo "📦  Installing API dependencies... "
+        pip3 install flask flask-restx python-dotenv requests >/dev/null
 fi
 
-if [ -d venv ]; then	
-   
-   echo -e "🐍  Activating Python virtual environment... "
-   source venv/bin/activate
-   pip3 freeze | grep -E "flask-restx" > /dev/null
-   if [ ! $? -ne 0 ]; then
-      echo "📦  Installing API dependencies... "
-      pip3 install flask flask-restx python-dotenv requests > /dev/null
-   fi
-else
-   echo "🕒 Craeting Python virtual environment... "
-   python3 -m venv venv
-   echo -e "🐍  Activating Python virtual environment... "
-   source venv/bin/activate
-   
-   echo "📦  Installing API dependencies... "
-   pip3 install flask flask-restx python-dotenv requests > /dev/null 
+if [ $? -ne 0 ]; then
+        echo -e "❌  package python3-venv is not installed!\n"
+        exit 1
 fi
 
 echo -e "🔛  Start API in port 8181\n"
