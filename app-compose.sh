5d7d26ed28a89dda9b33ade8e6553846a6ee1648
caio86 <caioluiz86@gmail.com>
2024-10-21 20:10:42 -0300

    refactor: update app-compose.sh
    -- Replaced method to obtain host IP with `ip -4 route get 1.1.1.1` for more accurate retrieval.
    -- Switched from `docker-compose` to `docker compose` commands for modern Docker syntax.

--- app-compose.sh
+++ app-compose.sh
@@ -3,29 +3,31 @@
 
 OPTION=$1
 
-echo "IP_HOST_API=$(hostname -I | cut -d" " -f1)" > ./.env
+echo "IP_HOST_API=$(ip -4 route get 1.1.1.1 | awk '{print $7}' | head -n1)" >./.env
 ENVFILE=$PWD/.env
 if [ ! -s "$ENVFILE" ]; then
-        echo "File .env does not exist or is empty!"; exit
+        echo "File .env does not exist or is empty!"
+        exit
 fi
 
 case $OPTION in
-        "--start")
-                echo -e '🐳  Building containers ...'
-                docker-compose build > /dev/null
-                echo -e '🐳  Starting containers ...'
-                docker-compose up -d > /dev/null
-                echo -e '🚀  container started successfully!'
-                echo "🕒 Initilize API Provision ... "
-                bash start-api.sh
-                ;;
-        "--destroy")
-                echo -e '🔴  Destroying containers and images ...'
-                docker-compose down --rmi all
-                echo "" > .env
-                echo -e '🤝  Finished environment ...'
-                ;;
-        *)
-                echo "Not a valid argument!"
-                echo "excution example: ./app-compose.sh <option>[--start|--destroy]"
+"--start")
+        echo -e '🐳  Building containers ...'
+        docker compose build >/dev/null
+        echo -e '🐳  Starting containers ...'
+        docker compose up -d >/dev/null
+        echo -e '🚀  container started successfully!'
+        echo "🕒 Initilize API Provision ... "
+        bash start-api.sh
+        ;;
+"--destroy")
+        echo -e '🔴  Destroying containers and images ...'
+        docker-compose down --rmi all
+        echo "" >.env
+        echo -e '🤝  Finished environment ...'
+        ;;
+*)
+        echo "Not a valid argument!"
+        echo "excution example: ./app-compose.sh <option>[--start|--destroy]"
+        ;;
 esac
